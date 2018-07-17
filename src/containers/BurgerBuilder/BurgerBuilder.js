167590e2d46ef7cf4706b6005295a36dcf331dc5
Matthieu <matthieunadeau96@gmail.com>
2018-07-17 14:03:38 -0400

    Created Axios Instance & user can now send order to Firebase

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,6 +5,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
   lettuce: 0.25,
@@ -83,7 +84,24 @@ class BurgerBuilder extends Component {
   }
 
   purchaseContinueHandler = () => {
-    alert("You continued!");
+    // alert("You continued!");
+    const order = {
+      ingredients: this.state.ingredients,
+      price: this.state.totalPrice,
+      customer: {
+        name: 'Matthieu Nadeau',
+        address: {
+          street: 'testStreet 1',
+          zipCode: '12345',
+          country: 'US'
+        },
+        email: 'test@test.com'
+      },
+      deliveryMethod: 'premium'
+    }
+    axios.post('/orders.json', order)
+      .then(response => console.log(response))
+      .catch(error => console.log(error));
   }
 
   render () {
